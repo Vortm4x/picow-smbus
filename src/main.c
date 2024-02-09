994577a70469cd9ef641e1924bd32d34f1549175
Maksym Trehubenko <maksym.trehubenko@globallogic.com>
2024-02-09 23:42:50 +0200

    [read byte] / [quick ON] are handled before writing to master

--- src/main.c
+++ src/main.c
@@ -10,63 +10,141 @@
 #include "smbus_slave.h"
 
 
-#define PICO_I2C_SLAVE_INSTANCE        i2c0
-#define PICO_I2C_SLAVE_SDA_PIN         12
-#define PICO_I2C_SLAVE_SCL_PIN         13
-#define PICO_I2C_SLAVE_ADDRESS         0x17
-#define PICO_I2C_SLAVE_BAUDRATE        100000
+#define PICO_SMBUS_SLAVE_I2C_INSTANCE    i2c0
+#define PICO_SMBUS_SLAVE_I2C_ADDRESS     0x17
+#define PICO_SMBUS_SLAVE_BAUDRATE        50000
+#define PICO_SMBUS_SLAVE_SMDAT_PIN       12
+#define PICO_SMBUS_SLAVE_SMCLK_PIN       13
+
+#define SMBUS_BYTE_CMD 0xC0
+#define SMBUS_BYTE_DATA_CMD 0xC1
+#define SMBUS_WORD_DATA_CMD 0xC2
+#define SMBUS_BLOCK_DATA_CMD 0xCB
 
 
-void quick_handler(bool value);
-void read_byte_handler(uint8_t* byte);
-void undo_read_byte_handler();
-void pico_smbus_slave_init();
-bool init_all();
+static void quick_handler(bool value);
+static void read_byte_handler(uint8_t* byte);
+static void write_byte_handler(uint8_t* byte);
+static void read_data_handler(uint8_t command, smbus_data_t* smbus_data);
+static void write_data_handler(uint8_t command, smbus_data_t* smbus_data);
+
+static void pico_smbus_slave_init();
+static bool init_all();
+
+
+// #define SMBUS_PICO_DBG
+
+#ifdef SMBUS_PICO_DBG
+    #define PICO_PRINT(...) printf(__VA_ARGS__)
+#else
+    #define PICO_PRINT(...)
+#endif
 
 
 void quick_handler(bool value)
 {
     if(value)
     {
-        printf("Quick ON\n");
+        PICO_PRINT("QUICK ON\n");
     }
     else
     {
-        printf("Quick OFF\n");
+        PICO_PRINT("QUICK OfF\n");
     }
 }
 
 void read_byte_handler(uint8_t* byte)
 {
-    *byte = 0xFC; 
-    printf("Read byte\n");
+    *byte = SMBUS_BYTE_CMD;
+    PICO_PRINT("[TX] byte: 0x%02X\n", *byte);
 }
 
-void undo_read_byte_handler()
+void write_byte_handler(uint8_t* byte)
 {
-    printf("Undo Read Byte\n");
+    PICO_PRINT("[RX] byte: 0x%02X\n", *byte);
+}
+
+void read_data_handler(uint8_t command, smbus_data_t* smbus_data)
+{
+    switch (command)
+    {
+        case SMBUS_BYTE_DATA_CMD:
+        {
+            smbus_data->byte = 0x11;
+            PICO_PRINT("[TX] byte data: 0x%02X\n", smbus_data->byte);
+        }
+        break;
+    
+        case SMBUS_WORD_DATA_CMD:
+        {
+            smbus_data->word = 0x1122;
+            PICO_PRINT("[TX] word data: 0x%04X\n", smbus_data->word);
+        }
+        break;
+
+        case SMBUS_BLOCK_DATA_CMD:
+        {
+            PICO_PRINT("[TX] block data:");
+
+            for(int i = 0; i < SMBUS_MAX_BLOCK_LEN; ++i)
+            {
+                smbus_data->block[i] = i;
+                PICO_PRINT(" 0x%02X", smbus_data->block[i]);
+            }
+
+            PICO_PRINT("\n");
+        }
+        break;
+    }   
+}
+
+void write_data_handler(uint8_t command, smbus_data_t* smbus_data)
+{
+    switch (command)
+    {
+        case SMBUS_BYTE_DATA_CMD:
+        {
+            PICO_PRINT("[RX] byte data: 0x%02X\n", smbus_data->byte);
+        }
+        break;
+    
+        case SMBUS_WORD_DATA_CMD:
+        {
+            PICO_PRINT("[RX] word data: 0x%04X\n", smbus_data->word);
+        }
+        break;
+
+        case SMBUS_BLOCK_DATA_CMD:
+        {
+            PICO_PRINT("[RX] block data:");
+
+            for(int i = 0; i < SMBUS_MAX_BLOCK_LEN; ++i)
+            {
+                PICO_PRINT(" 0x%02X", smbus_data->block[i]);
+            }
+            
+            PICO_PRINT("\n");
+        }
+        break;
+    }   
 }
 
 
 void pico_smbus_slave_init()
 {
-    gpio_init(PICO_I2C_SLAVE_SDA_PIN);
-    gpio_set_function(PICO_I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_I2C_SLAVE_SDA_PIN);
-
-    gpio_init(PICO_I2C_SLAVE_SCL_PIN);
-    gpio_set_function(PICO_I2C_SLAVE_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_I2C_SLAVE_SCL_PIN);
-
     smbus_slave_init(
-        PICO_I2C_SLAVE_INSTANCE, 
-        PICO_I2C_SLAVE_ADDRESS, 
-        PICO_I2C_SLAVE_BAUDRATE
+        PICO_SMBUS_SLAVE_I2C_INSTANCE, 
+        PICO_SMBUS_SLAVE_I2C_ADDRESS, 
+        PICO_SMBUS_SLAVE_BAUDRATE,
+        PICO_SMBUS_SLAVE_SMDAT_PIN,
+        PICO_SMBUS_SLAVE_SMCLK_PIN
     );
 
-    smbus_set_quick_handler(PICO_I2C_SLAVE_INSTANCE, quick_handler);
-    smbus_set_read_byte_handler(PICO_I2C_SLAVE_INSTANCE, read_byte_handler);
-    smbus_set_undo_read_byte_handler(PICO_I2C_SLAVE_INSTANCE, undo_read_byte_handler);
+    smbus_set_quick_handler(PICO_SMBUS_SLAVE_I2C_INSTANCE, quick_handler);
+    smbus_set_read_byte_handler(PICO_SMBUS_SLAVE_I2C_INSTANCE, read_byte_handler);
+    smbus_set_write_byte_handler(PICO_SMBUS_SLAVE_I2C_INSTANCE, write_byte_handler);
+    smbus_set_read_data_handler(PICO_SMBUS_SLAVE_I2C_INSTANCE, read_data_handler);
+    smbus_set_write_data_handler(PICO_SMBUS_SLAVE_I2C_INSTANCE, write_data_handler);
 }
 
 
@@ -97,7 +175,7 @@ int main()
         return -1;
     }
 
-    printf("Pico SMBUS slave started at 0x%02X\n", PICO_I2C_SLAVE_ADDRESS);
+    printf("Pico SMBUS slave started at 0x%02X\n", PICO_SMBUS_SLAVE_I2C_ADDRESS);
 
     while (true);
     
--- src/smbus_slave.c
+++ src/smbus_slave.c
@@ -1,8 +1,10 @@
 #include "smbus_slave.h"
 
 #include <hardware/irq.h>
+#include <hardware/gpio.h> 
 #include <string.h>
-#include <stdio.h>
+
+#include <stdio.h>  //debug only
 
 
 #define SMBUS_MIN_BAUD_RATE_HZ _u(10000)
@@ -14,20 +16,29 @@
         callback(__VA_ARGS__);          \
     }
 
+#define SMBUS_DBG
+
+#ifdef SMBUS_DBG
+    #define SMBUS_DBG_PUTCHAR(x) putchar(x)
+#else
+    #define SMBUS_DBG_PUTCHAR(x)
+#endif
+
 
 typedef struct smbus_slave_t
 {
-    bool is_quick_abort;
-    bool is_cmd_read;
-    bool is_restarted;
-
     quick_handler_t quick_handler;
     byte_handler_t read_byte_handler;
     byte_handler_t write_byte_handler;
     data_handler_t read_data_handler;
     data_handler_t write_data_handler;
 
-    undo_read_byte_handler_t undo_read_byte_handler;
+    uint scl_pin;
+    uint sda_pin;
+
+    bool is_cmd_read;
+    bool is_restarted;
+    bool is_quick_on;
 
     uint8_t cmd_byte;
     smbus_data_t smbus_data;
@@ -44,15 +55,18 @@ static void __isr __not_in_flash_func(smbus_slave_irq_tx_abrt)(smbus_slave_t* sl
 static void __isr __not_in_flash_func(smbus_slave_irq_rx_full)(smbus_slave_t* slave, i2c_inst_t* i2c);
 static void __isr __not_in_flash_func(smbus_slave_irq_rd_req)(smbus_slave_t* slave, i2c_inst_t* i2c);
 static void __isr __not_in_flash_func(smbus_slave_irq_handler)(void);
-
+static void smbus_init_i2c_gpio(uint gpio);
 
 void smbus_slave_irq_restart(smbus_slave_t* slave)
 {
+    SMBUS_DBG_PUTCHAR('R');
     slave->is_restarted = true;
 }
 
 void smbus_slave_irq_start(smbus_slave_t* slave)
 {
+    SMBUS_DBG_PUTCHAR('S');
+
     if(slave->is_restarted)
     {
         SMBUS_CALLBACK(slave->read_data_handler, slave->cmd_byte, &slave->smbus_data);
@@ -61,29 +75,24 @@ void smbus_slave_irq_start(smbus_slave_t* slave)
 
 void smbus_slave_irq_tx_abrt(smbus_slave_t* slave, uint32_t abrt_source)
 {
-    if((abrt_source & I2C_IC_TX_ABRT_SOURCE_ARB_LOST_BITS)
-    && (abrt_source & I2C_IC_TX_ABRT_SOURCE_ABRT_SLV_ARBLOST_BITS))
-    {
-        slave->is_quick_abort = true;
-        slave->is_cmd_read = false;
-        
-        SMBUS_CALLBACK(slave->undo_read_byte_handler);
-    }    
+    SMBUS_DBG_PUTCHAR('A');
 }
 
 void smbus_slave_irq_stop(smbus_slave_t* slave)
 {
     if(!slave->is_cmd_read)
     {
-        if(slave->is_quick_abort)
+        SMBUS_CALLBACK(slave->quick_handler, slave->is_quick_on);
+
+        if(slave->is_quick_on)
         {
-            SMBUS_CALLBACK(slave->quick_handler, true);
+            SMBUS_DBG_PUTCHAR('+');
         }
         else
         {
-            SMBUS_CALLBACK(slave->quick_handler, false);
+            SMBUS_DBG_PUTCHAR('-');
         }
-    }      
+    }
     else
     {
         if(slave->io_next_byte == 0)
@@ -96,17 +105,21 @@ void smbus_slave_irq_stop(smbus_slave_t* slave)
         }
     }  
 
-    slave->is_quick_abort = false;
     slave->is_cmd_read = false;
     slave->is_restarted = false;
-
+    slave->is_quick_on = false;
     slave->io_next_byte = 0;
     slave->cmd_byte = 0x00;
     memset(&slave->smbus_data, 0, sizeof(smbus_data_t));
+
+    SMBUS_DBG_PUTCHAR('P');
+    SMBUS_DBG_PUTCHAR('\n');
 }
 
 void smbus_slave_irq_rx_full(smbus_slave_t* slave, i2c_inst_t* i2c)
 {
+    SMBUS_DBG_PUTCHAR('w');
+
     if(slave->is_cmd_read)
     {
         slave->smbus_data.block[slave->io_next_byte] = i2c_read_byte_raw(i2c);
@@ -123,15 +136,27 @@ void smbus_slave_irq_rd_req(smbus_slave_t* slave, i2c_inst_t* i2c)
 {
     if(slave->is_cmd_read)
     {
+        SMBUS_DBG_PUTCHAR('r');
+
         i2c_write_byte_raw(i2c, slave->smbus_data.block[slave->io_next_byte]);
         slave->io_next_byte += 1;
     }
     else
     {
-        SMBUS_CALLBACK(slave->read_byte_handler, &slave->cmd_byte);
-        i2c_write_byte_raw(i2c, slave->cmd_byte);
+        if(gpio_get(slave->sda_pin))
+        {
+            SMBUS_DBG_PUTCHAR('r');
+            
+            SMBUS_CALLBACK(slave->read_byte_handler, &slave->cmd_byte);
+            slave->is_cmd_read = true;
+        }
+        else
+        {
+            slave->cmd_byte = 0xFF;
+            slave->is_quick_on = true;
+        }
 
-        slave->is_cmd_read = true;
+        i2c_write_byte_raw(i2c, slave->cmd_byte);
     }
 }
 
@@ -193,22 +218,38 @@ void smbus_slave_irq_handler(void)
     }
 }
 
+void smbus_init_i2c_gpio(uint gpio)
+{
+    gpio_init(gpio);
+    gpio_set_function(gpio, GPIO_FUNC_I2C);
+    gpio_pull_up(gpio);
+}
 
 void smbus_slave_init(
     i2c_inst_t* i2c, 
     uint8_t address, 
-    uint baudrate
+    uint baudrate,
+    uint sda_pin,
+    uint scl_pin
 )
 {
     assert(i2c == i2c0 || i2c == i2c1);
     assert(SMBUS_MIN_BAUD_RATE_HZ <= baudrate && baudrate <= SMBUS_MAX_BAUD_RATE_HZ);
+    
+    i2c_hw_t* hw = i2c_get_hw(i2c);
+
+    uint i2c_index = i2c_hw_index(i2c);
+    uint intr_num = I2C0_IRQ + i2c_index;
+    smbus_slave_t* slave = &smbus_slaves[i2c_index];
+
+    memset(slave, 0, sizeof(smbus_slave_t));
+
+    smbus_init_i2c_gpio(sda_pin);
+    smbus_init_i2c_gpio(scl_pin);
 
     i2c_init(i2c, baudrate);
     i2c_set_slave_mode(i2c, true, address);
     
-
-    i2c_hw_t* hw = i2c_get_hw(i2c);
-
     hw->intr_mask = I2C_IC_INTR_MASK_M_START_DET_BITS
                   | I2C_IC_INTR_MASK_M_RESTART_DET_BITS
                   | I2C_IC_INTR_MASK_M_TX_ABRT_BITS
@@ -216,35 +257,35 @@ void smbus_slave_init(
                   | I2C_IC_INTR_MASK_M_RX_FULL_BITS
                   | I2C_IC_INTR_MASK_M_RD_REQ_BITS;
 
-    uint i2c_index = i2c_hw_index(i2c);
-    uint intr_num = I2C0_IRQ + i2c_index;
-    
-    smbus_slave_t* slave = &smbus_slaves[i2c_index];
-    memset(slave, 0, sizeof(smbus_slave_t));
-    
     irq_set_exclusive_handler(intr_num, smbus_slave_irq_handler);
     irq_set_enabled(intr_num, true);
+
+    slave->sda_pin = sda_pin;
+    slave->scl_pin = scl_pin;
 }
 
 void smbus_slave_deinit(
     i2c_inst_t* i2c
-) 
+)
 {
     assert(i2c == i2c0 || i2c == i2c1);
 
+    i2c_hw_t *hw = i2c_get_hw(i2c);
+
     uint i2c_index = i2c_hw_index(i2c);
     uint intr_num = I2C0_IRQ + i2c_index;
-
     smbus_slave_t* slave = &smbus_slaves[i2c_index];
-    memset(slave, 0, sizeof(smbus_slave_t));
 
     irq_set_enabled(intr_num, false);
     irq_remove_handler(intr_num, smbus_slave_irq_handler);
-
-    i2c_hw_t *hw = i2c_get_hw(i2c);
     hw->intr_mask = I2C_IC_INTR_MASK_RESET;
 
     i2c_set_slave_mode(i2c, false, 0);
+    
+    gpio_deinit(slave->sda_pin);
+    gpio_deinit(slave->scl_pin);
+    
+    memset(slave, 0, sizeof(smbus_slave_t));
 }
 
 
@@ -288,14 +329,6 @@ void smbus_set_write_data_handler(i2c_inst_t* i2c, data_handler_t data_handler)
     slave->write_data_handler = data_handler;
 }
 
-void smbus_set_undo_read_byte_handler(i2c_inst_t* i2c, undo_read_byte_handler_t undo_read_byte_handler)
-{
-    uint i2c_index = i2c_hw_index(i2c);
-    smbus_slave_t* slave = &smbus_slaves[i2c_index];
-
-    slave->undo_read_byte_handler = undo_read_byte_handler;
-}
-
 void smbus_reset_handler(i2c_inst_t* i2c, smbus_slave_event_t slave_event)
 {
     uint i2c_index = i2c_hw_index(i2c);
@@ -318,9 +351,5 @@ void smbus_reset_handler(i2c_inst_t* i2c, smbus_slave_event_t slave_event)
         case SMBUS_SLAVE_WRITE_DATA:
             slave->write_data_handler = NULL;
             break;
-        case SMBUS_SLAVE_UNDO_READ_BYTE:
-            slave->undo_read_byte_handler = NULL;
-            break;
     }
 }
-
